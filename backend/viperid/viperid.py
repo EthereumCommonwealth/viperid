9f299cbbead980342449b0fa3d7a78856f26326f
Yohan Graterol <yohangraterol92@gmail.com>
2017-08-19 17:17:54 -0500

    Add compile_all method to the API

--- backend/viperid/viperid.py
+++ backend/viperid/viperid.py
@@ -20,7 +20,7 @@ def index():
 
 @app.route('/abi/', methods=['POST', ])
 @body_required
-def compiler_to_abi(code: str):
+def compile_to_abi(code: str):
     try:
         result = compiler.mk_full_signature(code)
     except Exception as e:
@@ -35,7 +35,7 @@ def compiler_to_abi(code: str):
 
 @app.route('/ir/', methods=['POST', ])
 @body_required
-def compiler_to_ir(code: str):
+def compile_to_ir(code: str):
     try:
         result = optimizer.optimize(parse_to_lll(code))
     except Exception as e:
@@ -50,7 +50,7 @@ def compiler_to_ir(code: str):
 
 @app.route('/bytecode/', methods=['POST', ])
 @body_required
-def compiler_to_bytecode(code: str):
+def compile_to_bytecode(code: str):
     try:
         result = '0x' + compiler.compile(code).hex()
     except Exception as e:
@@ -60,4 +60,35 @@ def compiler_to_bytecode(code: str):
 
     return jsonify(
         result=result
+    )
+
+
+@app.route('/compile/', methods=['POST',])
+@body_required
+def compile_all(code: str):
+    try:
+        result_abi = compiler.mk_full_signature(code)
+    except Exception as e:
+        return jsonify(
+            error=get_error(e)
+        ), 400
+
+    try:
+        result_ir = optimizer.optimize(parse_to_lll(code))
+    except Exception as e:
+        return jsonify(
+            error=get_error(e)
+        ), 400
+
+    try:
+        result_bytecode = '0x' + compiler.compile(code).hex()
+    except Exception as e:
+        return jsonify(
+            error=get_error(e)
+        ), 400
+
+    return jsonify(
+        result_abi=result_abi,
+        result_ir=result_ir,
+        result_bytecode=result_bytecode
     )
